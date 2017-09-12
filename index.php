cd1cbfddbe52236e781d233b21ce81f8c0a4305c
Alexandria Rull <ahlexrull@gmail.com>
2017-09-12 18:05:44 +0800

    Added responsive tables and fixed first revision of css

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ header('Location: login.php');
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.js"></script>
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
