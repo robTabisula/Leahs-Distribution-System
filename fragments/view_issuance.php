5569cc5f2ff2363fad6d302cf5db13cad5ad05ab
giolaranang <32018848+giolaranang@users.noreply.github.com>
2017-09-24 01:00:05 +0800

    did some adjusting to the issuance

--- fragments/view_issuance.php
+++ fragments/view_issuance.php
@@ -5,55 +5,110 @@
 
 <body>
 	<table id="datatables" class="table table-hover table-bordered dataTable" cellspacing="0" width="100%" role="grid" aria-describedby="myTable_info" style="width: 100%;">
+	<?php $choice=$_POST['choice'];?>
 		   <thead>
 	             <tr>
-	                <th>Product</th>
+	                <th>Product Name</th>
 	                <th>Quantity</th>
-	                <th>Price</th>
-	                <th>Client</th>
-	                <th>Date Issued</th>
-	                <th>Remarks</th>
+	                <th>Factory Unit Price</th>
+	                <th>Adjusted Price</th>
+	                <th>Total Unit Price</th>
 	             </tr>
 	       </thead>
 	       <tbody>
-		<?php
-			$choice=$_POST['choice'];
-		//edit table design
-			$retrieve = ("SELECT * FROM issuance_list il inner join issuance i inner join product_list pl inner join product_loc pc inner join clients c on il.issue_id = i.issue_id and il.prod_id = pl.productList_id and pl.productList_id = pc.product_id");
 
-			$results = mysqli_query($db, $retrieve);
-
-			while($issueRow=mysqli_fetch_array($results)){
-				$productName=$issueRow['productList_name'];
-				$qty=$issueRow['prod_qty'];
-				$price=$issueRow['prod_price'];
-				$client=$issueRow['c_name'];
-				$date_time=$$issueRow['issue_date_time'];
-				$remarks=$issueRow['remarks'];
-		?>
 				<tr>
 					<td data-title="productName">
-						<?php echo $productName;?>
+						<select name="productlist">
+					
+						</select>
 					</td>
 					<td data-title="quantity">
-						<?php echo $qty;?>
+						<input type="text" name="quantity"/>
 					</td>
 					<td data-title="price">
-						<?php echo $price;?>
+						<?php echo "under construction"?>
 					</td>
-					<td data-title="client">
-						<?php echo $client;?>
+					<td data-title="price">
+						<input type="text" name="adjusted_price"/>
 					</td>
-					<td data-title="dateTime">
-						<?php echo $date_time;?>
+					<td data-title="price">
+						<?php echo "under construction"?>
 					</td>
-					<td data-title="remarks">
-						<?php echo $remarks;?>
-					</td>
-
-		<?php
-				}
-		?>
+				</tr>
 			</tbody>
 			</table>
+
+			 <div class="panel-body">  
+                    <form role="form" method="post" action="fragments/issuance_fn.php">  
+                        <fieldset>  
+							
+							<h4>Issuance ID</h4>
+							<h4><Input type="label" id="output" name="issue_id"></input></h4>
+							<p><button id="generate">Generate</button></p>
+							
+							<div class="client">
+							<h4>Clients</h4>
+                                        <?php
+										$retrieveCat = ("SELECT c_id, c_name  FROM clients");
+										$clientRetrieve = mysqli_query($db, $retrieveCat);
+									?>
+
+                                    <select name="clientlist">
+				                        <?php
+											foreach ($clientRetrieve as $data):
+												$toData = $data["c_id"];
+										?>
+
+                                    	<option value = "<?= $data['c_name'] ?>"> <?php echo $data["c_name"]; ?></option>
+                                	  
+                                	   <?php
+											endforeach;
+										?>
+                                	</select>
+							</div>
+							<div class="remarks">
+                             <h4>Remarks</h4>
+                            <textarea rows="3" cols="30" name="remarks" ></textarea>
+							</div>
+							
+							<div class="dateTime">
+							<h4>Date and Time</h4> 
+                            <?php echo $date = date("Y-m-d H:i:s");  ?>
+                            <input type="hidden" name="date" value="<?php echo $date;?>"/>
+							</div>
+							
+                            <div class="form-group">							
+                                    <?php
+										$retrieveProd = ("SELECT productList_id, productList_name,productList_origprice FROM product_list");
+										$prodRetrieve = mysqli_query($db, $retrieveProd);
+									?>
+
+                                    <select name="productList">
+				                        <?php
+											foreach ($prodRetrieve as $data):
+												$toData = $data["productList_id"];
+										?>
+
+                                    	<option value = "<?= $data['productList_name'] ?>"> <?php echo $data["productList_name"]; ?></option>
+                                	  
+                                	   <?php
+											endforeach;
+										?>
+                                	</select>
+							
+								<?php echo $data ["productList_origprice"]; ?>
+								
+								<input placeholder="Adjusted Price" name="adjPrice" type="number">  
+								
+								<input placeholder="Quanity" name="quantity" type="number"  required>
+							  
+
+                            </div>
+							
+							<input class="btn btn-lg btn-success btn-block" type="submit" value="Save" name="add_issuance" > 
+  
+                        </fieldset>  
+                    </form>  
+                </div>   
 <body>
--- issuance.php
+++ issuance.php
@@ -189,10 +189,10 @@ if(!$_SESSION['username'])  {
             </table>
 
                 <center><select name="acctype" onchange="javascript:viewIssuance(this.value);" required>
-					<option value="none">Select...</option>
-					<option value="Regular">Regular</option>
-					<option value="Penthouse">Penthouse</option>
-					<option value="Others">Others</option>
+					<option value="1">Select...</option>
+					<option value="1">Regular</option>
+					<option value="2">Penthouse</option>
+					<option value="3">Others</option>
 				</select><center>
 				
                 <!--This is the div to show issuance-->
@@ -200,80 +200,8 @@ if(!$_SESSION['username'])  {
                 </div>
                 <!---->
                     
-                <!--Modal for the add button(not done)-->
-
-                <div class="panel-body">  
-                    <form role="form" method="post" action="fragments/issuance_fn.php">  
-                        <fieldset>  
-							
-							<h4>Issuance ID</h4>
-							<h4><code id="output" name="issue_id"></code></h4>
-							<p><button id="generate">Generate</button></p>
-							
-							<div class="client">
-							<h4>Clients</h4>
-                                        <?php
-										$retrieveCat = ("SELECT c_id, c_name  FROM clients");
-										$clientRetrieve = mysqli_query($db, $retrieveCat);
-									?>
-
-                                    <select name="clientlist">
-				                        <?php
-											foreach ($clientRetrieve as $data):
-												$toData = $data["c_id"];
-										?>
-
-                                    	<option value = "<?= $data['c_name'] ?>"> <?php echo $data["c_name"]; ?></option>
-                                	  
-                                	   <?php
-											endforeach;
-										?>
-                                	</select>
-							</div>
-							<div class="remarks">
-                             <h4>Remarks</h4>
-                            <textarea rows="3" cols="30" name="remarks" ></textarea>
-							</div>
-							
-							<div class="dateTime">
-							<h4>Date and Time</h4> 
-                            <?php echo $date = date("Y-m-d H:i:s");  ?>
-                            <input type="hidden" name="date" value="<?php echo $date;?>"/>
-							</div>
-							
-                            <div class="form-group">							
-                                    <?php
-										$retrieveProd = ("SELECT productList_id, productList_name,productList_origprice FROM product_list");
-										$prodRetrieve = mysqli_query($db, $retrieveProd);
-									?>
-
-                                    <select name="productList">
-				                        <?php
-											foreach ($prodRetrieve as $data):
-												$toData = $data["productList_id"];
-										?>
-
-                                    	<option value = "<?= $data['productList_name'] ?>"> <?php echo $data["productList_name"]; ?></option>
-                                	  
-                                	   <?php
-											endforeach;
-										?>
-                                	</select>
-							
-								<?php echo $data ["productList_origprice"]; ?>
-								
-								<input placeholder="Adjusted Price" name="adjPrice" type="number">  
-								
-								<input placeholder="Quanity" name="quantity" type="number"  required>
-							  
-
-                            </div>
-							
-							<input class="btn btn-lg btn-success btn-block" type="submit" value="Save" name="add_issuance" > 
-  
-                        </fieldset>  
-                    </form>  
-                </div>   
+                
+               
     </div>  
 </div>  
 
