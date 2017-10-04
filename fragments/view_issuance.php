ff156320fa12c6b515e3df39e5324d0531541e54
Robijn Tabisula <robijn753@gmail.com>
2017-10-05 01:57:50 +0800

    ...

--- fragments/view_issuance.php
+++ fragments/view_issuance.php
@@ -93,128 +93,128 @@ if(!$_SESSION['username'])  {
 .clonedInput ~ .clonedInput .add-row{ display:none; }
 </style>
 <body>
-	<?php
-		//variable for issuance categories
-		//1 for regular, 2 for penthouse, 3 for others
-			$choice=$_POST['choice'];
-		//regular issuance
-			if ($choice=='1'){
-	?>		    
-                <div class="panel-body">        				
+    <?php
+        //variable for issuance categories
+        //1 for regular, 2 for penthouse, 3 for others
+            $choice=$_POST['choice'];
+        //regular issuance
+            if ($choice=='1'){
+    ?>          
+                <div class="panel-body">                        
                     <form role="form" method="post" action="fragments/issuance_fn.php">  
                         <fieldset>  
 
-							<h4>Issuance ID</h4>
-									<?php
-										$retrieveId = ("SELECT issue_id from issuance order by 1 desc limit 1;");
-										$idRetrieve = mysqli_query($db, $retrieveId);
-										$idRow = mysqli_fetch_array($idRetrieve);
-
-										$latestid = $idRow['issue_id'];
-										$newID = $latestid + 1; //will increment 1 from the latest issuance ID
-									?>
-							<h4><input type="label" name="issue_id" value="<?php echo $newID;?>" readonly></input></h4>
-							
-							<div class="client">
-								<h4>Clients</h4>
+                            <h4>Issuance ID</h4>
                                     <?php
-										$retrieveCat = ("SELECT *  FROM clients");
-										$clientRetrieve = mysqli_query($db, $retrieveCat);
-									?>						
+                                        $retrieveId = ("SELECT issue_id from issuance order by 1 desc limit 1;");
+                                        $idRetrieve = mysqli_query($db, $retrieveId);
+                                        $idRow = mysqli_fetch_array($idRetrieve);
+
+                                        $latestid = $idRow['issue_id'];
+                                        $newID = $latestid + 1; //will increment 1 from the latest issuance ID
+                                    ?>
+                            <h4><input type="label" name="issue_id" value="<?php echo $newID;?>" readonly></input></h4>
+                            
+                            <div class="client">
+                                <h4>Clients</h4>
+                                    <?php
+                                        $retrieveCat = ("SELECT *  FROM clients");
+                                        $clientRetrieve = mysqli_query($db, $retrieveCat);
+                                    ?>                      
                                     <select name="clientlist" required>
                                     <option value = "" selected="true" disabled="disabled">Select Client..</option>
-				                        <?php
-											foreach ($clientRetrieve as $data):
-												$toData = $data["c_id"];
-										?>
+                                        <?php
+                                            foreach ($clientRetrieve as $data):
+                                                $toData = $data["c_id"];
+                                        ?>
 
-                                    	<option value = "<?php echo $data["c_id"];?>"> <?php echo $data["c_name"]; ?></option>
-                                	  
-                                	   <?php
-											endforeach;
-										?>
-                                	</select>
-							</div>
-							
-							<div class="remarks">
-								<h4>Remarks</h4>
-								<textarea rows="3" cols="30" name="remarks" ></textarea>
-							</div>
-							
-							<div class="dateTime">
-								<h4>Date and Time</h4> 
-									<?php $date = date("Y-m-d H:i:s");  ?>
-								<input type="label" name="date" value="<?php echo $date;?>" readonly/>
-							</div>
-							<br>
+                                        <option value = "<?php echo $data["c_id"];?>"> <?php echo $data["c_name"]; ?></option>
+                                      
+                                       <?php
+                                            endforeach;
+                                        ?>
+                                    </select>
+                            </div>
+                            
+                            <div class="remarks">
+                                <h4>Remarks</h4>
+                                <textarea rows="3" cols="30" name="remarks" ></textarea>
+                            </div>
+                            
+                            <div class="dateTime">
+                                <h4>Date and Time</h4> 
+                                    <?php $date = date("Y-m-d H:i:s");  ?>
+                                <input type="label" name="date" value="<?php echo $date;?>" readonly/>
+                            </div>
+                            <br>
 
-							<select name="branch" onchange="javascript:viewPrice(this.value);" required>
-									<option value="" selected="true" disabled="disabled">Select an Area</option>
-									<option value="Baguio">Baguio</option>
-									<option value="Pangasinan">Pangasinan</option>
-							</select>
-							<br>
-							<br>
-							
-                            <div class="form-group">							
+                            <select name="branch" onchange="javascript:viewPrice(this.value);" required>
+                                    <option value="" selected="true" disabled="disabled">Select an Area</option>
+                                    <option value="Baguio">Baguio</option>
+                                    <option value="Pangasinan">Pangasinan</option>
+                            </select>
+                            <br>
+                            <br>
+                            
+                            <div class="form-group">                            
                                     <?php
-										$retrieveProd = ("SELECT distinct productList_id, productList_name, category_id FROM product_list p inner join product_loc l on p.productList_id=l.product_id where status!='Disabled'");
-										$prodRetrieve = mysqli_query($db, $retrieveProd);
-									?>
+                                        $retrieveProd = ("SELECT distinct productList_id, productList_name, category_id FROM product_list p inner join product_loc l on p.productList_id=l.product_id where status!='Disabled'");
+                                        $prodRetrieve = mysqli_query($db, $retrieveProd);
+                                    ?>
                                 
-                            	<!--Div where clones go-->
-                            		<div id = "next">
-                            		</div>					
-					           <!--Div to view adjusted price and category-->
+                                <!--Div where clones go-->
+                                    <div id = "next">
+                                    </div>                  
+                               <!--Div to view adjusted price and category-->
                                 <div id="AdjustedPriceDiv" style="position : fixed; padding: 5px 0 0 5px; height: 140px; width: 150px; top:10%;  width: 300px; height: 150px; border: 1px solid blue; box-sizing: border-box; background: none no-repeat scroll 0 0 #fff;">
                                         <hr>
                                         <h4>When Choosing a product, Information will be viewed here.</h4>
                                         <hr>
                                 </div>  
-                            </div>	
+                            </div>  
                             <!--div to clone-->
-							<div id="clonedInput1" class="clonedInput">
-							   <br>
-											<select name="productList[]" id="productselect" onchange ="javascript:viewCategory(this.value);" required>
-															<option value = "" selected="true" disabled="disabled">Choose Product..</option>
-														<?php
-															foreach ($prodRetrieve as $datas):
-															$sproduct_id = $datas["productList_id"];
-														?>  
-															<option value = "<?php echo $sproduct_id;?>">
-															   <?php echo $datas["productList_name"]; ?>
-															</option>
-												  
-														<?php
-															endforeach;
-														?>
-											  </select>                              
-										<input placeholder="Adjusted Price" type="number" name="adjusted_price[]" required/> 
-										<input placeholder="Quantity" name="quantity[]" type="number"  required>
-										<input placeholder="Remarks" name="premarks[]"/>      
-								<div class="actions">
-									<input type="button" class="add-row" value="Add Product"/>  
-									<input type="button" class="remove" value="Remove"/>
-								</div>
-							</div> 
+                            <div id="clonedInput1" class="clonedInput">
+                               <br>
+                                            <select name="productList[]" id="productselect" onchange ="javascript:viewCategory(this.value);" required>
+                                                            <option value = "" selected="true" disabled="disabled">Choose Product..</option>
+                                                        <?php
+                                                            foreach ($prodRetrieve as $datas):
+                                                            $sproduct_id = $datas["productList_id"];
+                                                        ?>  
+                                                            <option value = "<?php echo $sproduct_id;?>">
+                                                               <?php echo $datas["productList_name"]; ?>
+                                                            </option>
+                                                  
+                                                        <?php
+                                                            endforeach;
+                                                        ?>
+                                              </select>                              
+                                        <input placeholder="Adjusted Price" type="number" name="adjusted_price[]" required/> 
+                                        <input placeholder="Quantity" name="quantity[]" type="number"  required>
+                                        <input placeholder="Remarks" name="premarks[]"/>      
+                                <div class="actions">
+                                    <input type="button" class="add-row" value="Add Product"/>  
+                                    <input type="button" class="remove" value="Remove"/>
+                                </div>
+                            </div> 
                 <!--/div to clone-->
-							    <input class="btn btn-lg btn-success btn-block" type="submit" value="Save" name="add_issuance"/>
-	                       	</fieldset>  
+                                <input class="btn btn-lg btn-success btn-block" type="submit" value="Save" name="add_issuance"/>
+                            </fieldset>  
                             <input name="choice" value="<?php echo $choice;?>"type="hidden"></input>
-                    	</form>  
-                	</div>   
-    			</div>  
-			</div>  
+                        </form>  
+                    </div>   
+                </div>  
+            </div>  
        </div>
     </div>
 <?php 
 //end of regular issuance
 }else if ($choice=='2'){
 //penthouse issuance
-	echo "not yet done";	
+    echo "not yet done";    
 }else if ($choice=='3'){
 //other issuance
-	echo "still under construction";
+    echo "still under construction";
 } 
 ?>
 </body>
