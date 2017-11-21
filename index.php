c29b92239d6eb2e6bea8d20b404edb8b853bf0db
Alexandria Rull <ahlexrull@gmail.com>
2017-11-22 02:18:01 +0800

    Added Index.php

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ if(!$_SESSION['username'])  {
     <?php include('fragments/config.php') ?>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="src/css/index.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.js"></script>
@@ -46,12 +47,11 @@ if(!$_SESSION['username'])  {
                 responsive: true
             });
         });
-
     </script>
 </head>
 
 <body>
-         <!-- Sidebar -->
+    <!-- Sidebar -->
     <!-- class="collapsed active" -->
     <div class="nav-side-menu">
         <div class="brand">
@@ -78,9 +78,9 @@ if(!$_SESSION['username'])  {
                         <i class="fa fa-dashboard fa-lg"></i> Dashboard
                     </a>
                 </li>
-				
+
                 <!-- Settings Submenu -->
-                 <li><a href="settings.php"><i class="fa fa-cog"></i> Me</a></li>
+                <li><a href="settings.php"><i class="fa fa-cog"></i> Me</a></li>
 
 
                 <!-- Accounts Submenu -->
@@ -100,7 +100,7 @@ if(!$_SESSION['username'])  {
                     <li> <a href="reports_Client.php"><i class="fa fa-table" aria-hidden="true"></i> Client Reports </a></li>
                     <li> <a href="reports_Product.php"><i class="fa fa-table" aria-hidden="true"></i> Product Reports </a></li>
                 </ul>
-                
+
                 <!-- Issuance Log Submenu -->
                 <li data-toggle="collapse" data-target="#issue" class="collapsed">
                     <i class="fa fa-list" aria-hidden="true"></i> Logs <span class="arrow"></span>
@@ -115,10 +115,10 @@ if(!$_SESSION['username'])  {
                     <li> <a href="log_Activity.php"><i class="fa fa-list-alt" aria-hidden="true"></i> Activity Logs </a></li>
 
                 </ul>
-                
+
                 <!-- Issuance menu -->
-                
-                 <li data-toggle="collapse" data-target="#issuance" class="collapsed">
+
+                <li data-toggle="collapse" data-target="#issuance" class="collapsed">
                     <i class="fa fa-id-card" aria-hidden="true"></i>Issuance<span class="arrow"></span>
                 </li>
                 <ul class="sub-menu collapse atarget" id="issuance">
@@ -135,7 +135,7 @@ if(!$_SESSION['username'])  {
                     <a class="sub-menu_nc" href="categories.php">
                         <i class="fa fa-archive"> </i> Category List
                     </a>
-                </li>   
+                </li>
                 <li class="sub-menu_nc">
                     <a class="sub-menu_nc" href="products.php">
                         <i class="fa fa-shopping-basket"> </i> Product List
@@ -152,121 +152,242 @@ if(!$_SESSION['username'])  {
     <!-- /#sidebar-wrapper -->
 
     <!-- /#Main Content -->
-	    <!-- Main Container -->
+    <!-- Main Container -->
     <div id="page-content-wrapper">
         <div class="containers">
-            <table class="table table-striped table-bordered">
-                <h1 align="center">Purchase Order</h1>
-            </table>
+            <!-- /span-3 -->
+            <div class="col-md-12">
 
-            <!-- Retrieve Account Data -->
-            <?php
-							$retrieve = ("SELECT * FROM purchased_order INNER JOIN clients ON purchased_order.client_id = clients.c_id");
-							$results = mysqli_query($db, $retrieve);
-						?>
+                <!-- Right -->
 
-                <table class="table table-striped table-bordered">
-                    <a href="purchased_order.php"><button type="button" class="btn btn-info btn-lg" data-toggle="modal">Add Order</button></a>
-                </table>
+                <strong><span class="glyphicon glyphicon-dashboard"></span> My Dashboard</strong>
 
-                <!-- Table Display for Accounts -->
-					<table id="datatables" class="table table-hover table-bordered dataTable" cellspacing="0" width="100%" role="grid" aria-describedby="myTable_info" style="width: 100%;">
-						<thead>
-							<tr>
-								<th>Ordered Products</th>
-								<th>Client</th>
-								<th>Merchandiser</th>
-								<th>Date and Time</th>
-								<th>Branch</th>
-							</tr>
-						</thead>
-			   
+                <hr>
 
-						<tbody>
-							<?php
-								foreach ($results as $data):
-									$toData = $data["order_id"];
-							?>
-								<tr>
-										<?php
-											$po_id=$data["order_id"];
-										?>
+                <div class="row" id="head">
+                    <div class="col-md-12">
+                        <!-- Left -->
+                        <div id="stat_info">
+                            <div class="well" id="prod_mostb">
 
-									<td data-title="Products Ordered">
-                                        <table class="table table-striped table-bordered">
-                                            <button type="button" class="glyphicon glyphicon-apple" data-toggle="modal" aria-hidden="true" data-target="#<?php echo $po_id ?>"></button>
-                                        </table>
-									</td>
-									
-									<td data-title="Client">
-										<?php echo $data["c_name"]; ?>
-									</td>
-									
-									<td data-title="Merchandiser">
-										<?php echo $data["merchandiser"]; ?>
-									</td>
-									
-									<td data-title="Date/Time">
-										<?php echo $data["order_date"]; ?>
-									</td>
-									
-									<td data-title="Branch">
-										<?php 
-										$passBranch = $data["c_location"];  
-                                        echo $passBranch;
-										?>
-									</td>
-									
-								</tr>
-								
-								<!-- Products Issued -->
-									<div id="<?php echo  $po_id ?>" class="modal fade" role="dialog">
-										<div class="modal-dialog modal-lg">
+                                <?php 
+                                    $queryProdB= "SELECT productList_name as 'name', count(issuance_list.issue_id) AS 'MAGNITUDE' FROM clients INNER JOIN issuance ON  clients.c_id = issuance.client_id INNER JOIN issuance_list ON issuance.issue_id = issuance_list.issue_id INNER JOIN product_list ON productList_id = issuance_list.prod_id WHERE c_location= 'Pangasinan' GROUP BY productList_name ORDER BY MAGNITUDE DESC;";
+                                    $runpb = mysqli_query($db, $queryProdB);
+                                    $thisprodb=mysqli_fetch_array($runpb);
 
-											<div class="modal-content">
-												<div class="modal-header">
-													<button type="button" class="close" data-dismiss="modal">&times;</button>
-													<h4 class="modal-title">Purchase Ordered</h4>
-												</div>
-												<div class="modal-body">
-														<h4>Purchased Order ID: <?php  echo $data["order_id"];  ?></h4>
-														<?php
-															$queryProducts = "SELECT * FROM  purchased_order_list INNER JOIN product_list ON purchased_order_list.prdct_id = product_list.productList_id INNER JOIN product_loc ON purchased_order_list.prdct_id = product_loc.product_id WHERE p_order_id = '$po_id' AND location = '$passBranch'";
-															$run = mysqli_query($db, $queryProducts);
-														?>
-														<label>Product</label>
-														&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-														&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-														<label>Quantity</label>
-														&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-														&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-														
-														<?php
-														
-															foreach ($run as $log){
-															$toData = $log["productList_id"];        
-														?>
-															<br><input type="text" value= "<?php  echo $log["productList_name"];  ?>" readonly>
-																<input type="text" value= "<?php  echo $log["order_qty"];  ?>" readonly>
+                                    $prodb_name = $thisprodb['name'];
 
-														<?php
-															}
-														
-														?>
-												   
-												</div>
-											</div>
-										</div>
-									</div>
-				
+                                ?>
 
-								<?php
-								endforeach;
-							?>
-						</tbody>
-					</table>
-		</div>
+                                <h4>Most Requested Product (Pangasinan)</h4>
+                                <p>
+                                    <?php echo $prodb_name;?>
+                                </p>
+
+                            </div>
+
+                            <div class="well" id="prod_mostp">
+                                <?php 
+                            $queryProdP= "SELECT productList_name as 'name', count(issuance_list.issue_id) AS 'MAGNITUDE' FROM clients INNER JOIN issuance ON  clients.c_id = issuance.client_id INNER JOIN issuance_list ON issuance.issue_id = issuance_list.issue_id INNER JOIN product_list ON productList_id = issuance_list.prod_id WHERE c_location='Pangasinan' GROUP BY productList_name ORDER BY MAGNITUDE DESC;";
+                            $runpp = mysqli_query($db, $queryProdP);
+                            $thisprodp=mysqli_fetch_array($runpp);
+
+                            $prodp_name = $thisprodp['name'];
+
+                        ?>
+
+                                <h4>Most Requested Product (Baguio)</h4>
+                                <p>
+                                    <?php echo $prodp_name;?>
+                                </p>
+                            </div>
+
+                            <div class="well" id="client_mostb">
+                                <?php 
+                            $queryClientB= "SELECT c_name, COUNT(issuance_list.issue_id) AS 'MAGNITUDE' FROM clients INNER JOIN issuance ON  clients.c_id = issuance.client_id INNER JOIN issuance_list ON issuance.issue_id = issuance_list.issue_id WHERE c_location='Baguio' GROUP BY c_name ORDER BY MAGNITUDE DESC LIMIT 1";
+                            $runclb = mysqli_query($db, $queryClientB);
+                            $thisclientb=mysqli_fetch_array($runclb);
+
+                            $clientb_name = $thisclientb['c_name'];
+
+                        ?>
+
+                                <h4>Most Active Client (Baguio)</h4>
+                                <p>
+                                    <?php echo $clientb_name;?>
+                                </p>
+                            </div>
+
+                            <div class="well" id="client_mostp">
+
+                                <?php 
+
+                            $queryClientP = "SELECT c_name, COUNT(issuance_list.issue_id) AS 'MAGNITUDE' FROM clients INNER JOIN issuance ON  clients.c_id = issuance.client_id INNER JOIN issuance_list ON issuance.issue_id = issuance_list.issue_id WHERE c_location='Pangasinan' GROUP BY c_name ORDER BY MAGNITUDE DESC LIMIT 1";
+                            $runclp = mysqli_query($db, $queryClientP);
+                            $thisclientp=mysqli_fetch_array($runclp);
+                        
+                            $clientp_name = $thisclientp['c_name'];
+
+
+                        ?>
+                                <h4>Most Active Client (Pangasinan)</h4>
+                                <p>
+                                    <?php echo $clientp_name;?>
+                                </p>
+                            </div>
+
+                            <div id="client_mostrec">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="panel">
+                            <div class="panel-heading">
+                                <i class="icon icon-chevron-up chevron"></i>
+                                <i class="icon icon-wrench pull-right"></i> Quick Start
+                            </div>
+                            <div class="panel-content">
+
+                                <div class="btn-group btn-group-justified">
+                                    <a href="issuance.php" class="btn btn-primary col-sm-3">
+                                        <i class="glyphicon glyphicon-plus"></i>
+                                        <p>Create Issuance</p>
+                                    </a>
+                                    <a href="log_Issuance.php" class="btn btn-primary col-sm-3">
+                                        <i class="glyphicon glyphicon-book"></i>
+                                        <p>Issuance Logs</p>
+                                    </a>
+                                    <a href="porder.php" class="btn btn-primary col-sm-3">
+                                        <i class="glyphicon glyphicon-arrow-down"></i>
+                                        <p>View Low Stocks</p>
+                                    </a>
+                                    <a href="log_Activity.php" class="btn btn-primary col-sm-3">
+                                        <i class="glyphicon glyphicon-cog"></i>
+                                        <p>Activity Logs</p>
+                                    </a>
+                                </div>
+                            </div>
+                            <!--/panel content-->
+                        </div>
+                        <!--/panel-->
+                        
+                        <hr>
+                        
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Low Stock Items 
+                            
+                            </div>
+
+                            <table class="table table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Product Name</th>
+                                        <th>Restock Level</th>
+                                        <th>Product Qty</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                   
+                                    <?php 
+
+                                        $sess_name = $_SESSION['username'];
+
+                                        $queryuser= "SELECT branch FROM accounts where username = '$sess_name';";
+                                        $runname = mysqli_query($db, $queryuser);
+                                        $thisuser=mysqli_fetch_array($runname);
+
+                                        $clientloc = $thisuser['branch'];
+
+                                        $queryLowStocks= "SELECT iS_inventoryid,productList_name, iS_restock_lvl, iS_quantity, (iS_restock_lvl-iS_quantity) AS 'DIFF' FROM inventory INNER JOIN product_list ON inventory.iS_product_id = product_list.productList_id where iS_quantity < iS_restock_lvl AND is_location= '$clientloc' ORDER BY DIFF DESC;";
+                                        $runls = mysqli_query($db, $queryLowStocks);
+                                       
+                                    
+                                     ?>
+                                        
+                                    <?php    
+
+                                        foreach($runls AS $product):    
+                                    
+
+                                    ?>
+                                    <tr>
+                                        <td>
+                                             <?php echo $product['productList_name']; ?>
+                                        </td>
+                                        <td>
+                                             <?php echo $product['iS_restock_lvl']; ?>
+                                        </td>
+                                        <td>
+                                             <?php echo $product['iS_quantity']; ?>
+                                        </td>
+                                    </tr>
+                                    
+                                     <?php
+                                        endforeach;
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!--/panel-->
+
+
+
+                        <!--/panel-->
+
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">Welcome!</div>
+                            <div class="panel-body">
+                                Hello there, 
+                                <?php
+                                echo $_SESSION['username'];
+                                ?>! 
+                            <br>
+                                Today is
+                                <?php
+                                    $now = new DateTime();
+                                    echo $now->format('Y-m-d H:i:s');
+                                ?>
+                            </div>
+                        </div>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <div class="panel-title">
+                                    <i class="glyphicon glyphicon-chevron-up chevron"></i>Charts
+                                </div>
+                            </div>
+                            <div class="panel-body">
+
+
+
+                            </div>
+                            <!--/panel content-->
+                        </div>
+                        <!--/panel-->
+
+
+                        <i class="icon-bar-chart icon-3x"></i>
+                        <i class="icon-plus icon-3x"></i>
+                        <i class="icon-facebook icon-3x"></i>
+                        <i class="icon-beaker icon-3x"></i>
+                        <i class="icon-twitter icon-3x"></i>
+
+
+                    </div>
+                    <!--/col-span-6-->
+
+                </div>
+                <!--/row-->
+            </div>
+            <!--/col-span-9-->
+        </div>
     </div>
+    <!-- /Main -->
 
 </body>
 
