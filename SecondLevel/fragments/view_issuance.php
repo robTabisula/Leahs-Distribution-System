e3d00d6c01d61708de0a84218ee03b9457cb5c9c
Robijn Tabisula <robijn753@gmail.com>
2018-01-11 01:23:45 +0800

    Sucurity Code Working (Issuance)

--- SecondLevel/fragments/view_issuance.php
+++ SecondLevel/fragments/view_issuance.php
@@ -432,6 +432,11 @@
 
                                 <hr style = "border-top: 3px double #8c8b8b;">
                                 <br>
+                                <div>
+                                    <h4>Security Code</h4>
+                                    <input type="password" name="secPass" placeholder="Enter Security Code" maxlength="5" required>
+                                </div>
+                                <br>
 
                                 <!--********************************************************************************** -->
                                     <input class="btn btn-lg btn-success btn-block" type="submit" value="Save" name="add_issuance"/>
@@ -571,6 +576,11 @@
 
 
                                 <hr style = "border-top: 3px double #8c8b8b;">
+                                <br>
+                                <div>
+                                    <h4>Security Code</h4>
+                                    <input type="password" name="secPass" placeholder="Enter Security Code" maxlength="5" required>
+                                </div>
                                 <br>
 
                                 <!--********************************************************************************** -->
@@ -721,6 +731,11 @@
 
 
                                 <hr style = "border-top: 3px double #8c8b8b;">
+                                <br>
+                                <div>
+                                    <h4>Security Code</h4>
+                                    <input type="password" name="secPass" placeholder="Enter Security Code" maxlength="5" required>
+                                </div>
                                 <br>
 
                                 <!--********************************************************************************** -->
